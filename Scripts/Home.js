aa2139b93f50b4016da66a8d3e863dbb4d0ff13f
koredeowoblow <100287793+koredeowoblow@users.noreply.github.com>
2025-01-17 03:12:47 +0100

    major

--- Scripts/Home.js
+++ Scripts/Home.js
@@ -1,177 +1,144 @@
-const username = sessionStorage.getItem('username');
+const username = sessionStorage.getItem('username')
 console.log(username)
 $('.user-name').html(username)
 
 document.querySelector('.eye-icon').addEventListener('click', async () => {
-  const balanceElement = document.querySelector('.balance');
-  const isHidden = balanceElement.textContent === '••••••';
+  const balanceElement = document.querySelector('.balance')
+  const isHidden = balanceElement.textContent === '••••••'
 
   if (isHidden) {
-    const balance = await fetchBalance();
-    balanceElement.textContent = balance !== null ? balance : 'Error';
+    const balance = await fetchBalance()
+    balanceElement.textContent = balance !== null ? balance : 'Error'
   } else {
-    balanceElement.textContent = '••••••';
+    balanceElement.textContent = '••••••'
   }
-});
+})
 
-async function fetchBalance() {
-
-  const token = sessionStorage.getItem("token");
-  const id = sessionStorage.getItem("user_id");
- console.log(token);
- console.log(id)
+async function fetchBalance () {
+  const token = sessionStorage.getItem('token')
+  const id = sessionStorage.getItem('user_id')
+  console.log(token)
+  console.log(id)
   if (!token || !id) {
-    console.error("Token or user ID is missing");
-    return;
+    console.error('Token or user ID is missing')
+    return
   }
 
   $.ajax({
-    type: "POST",
-    url: "https://testing1-xpjd.onrender.com/api/users/accountbalance",
+    type: 'POST',
+    url: 'https://testing1-xpjd.onrender.com/api/users/accountbalance',
     headers: {
-      'Authorization': `Bearer ${token}`
+      Authorization: `Bearer ${token}`
     },
     data: {
-      'userId': id
+      userId: id
     },
     success: function (data) {
       if (data.success === true) {
-        $(".balance").html(data.data.account_balance);
+        $('.balance').html("₦" + data.data.account_balance);
+
       } else {
-        console.error("Unexpected response:", data);
+        console.error('Unexpected response:', data)
       }
     },
     error: function (xhr, status, error) {
-      console.error("AJAX Error:", status, error);
+      console.error('AJAX Error:', status, error)
     }
-  });
-};
-
-// To enable the footer links to change color when clicked
-document.querySelectorAll('.footer-link').forEach((link) => {
-  link.addEventListener('click', (e) => {
-    document.querySelectorAll('.footer-link').forEach((el) => {
-      el.classList.remove('active');
-    });
-    link.classList.add('active');
-  });
-});
-
-// To enable the notifications button to change color when clicked  
-function toggleColor() {
-  const button = document.querySelector('.notifications-btn');
-  button.classList.toggle('clicked');
-  setTimeout(() => {
-    button.classList.remove('clicked');
-  }, 500);
+  })
 }
 
+// To enable the footer links to change color when clicked
+document.querySelectorAll('.footer-link').forEach(link => {
+  link.addEventListener('click', e => {
+    document.querySelectorAll('.footer-link').forEach(el => {
+      el.classList.remove('active')
+    })
+    link.classList.add('active')
+  })
+})
+
+// To enable the notifications button to change color when clicked
+function toggleColor () {
+  const button = document.querySelector('.notifications-btn')
+  button.classList.toggle('clicked')
+  setTimeout(() => {
+    button.classList.remove('clicked')
+  }, 500)
+}
 
 // Animation 1
 
-const childDiv = document.querySelector('.header');
-console.log("its working")
+const childDiv = document.querySelector('.header')
+console.log('its working')
 
 const handleScroll = () => {
-  const rect = childDiv.getBoundingClientRect();
+  const rect = childDiv.getBoundingClientRect()
   if (rect.top < window.innerHeight && rect.bottom >= 0) {
-    childDiv.classList.add('in-view');
+    childDiv.classList.add('in-view')
   }
-};
+}
 
-window.addEventListener('scroll', handleScroll);
-handleScroll(); // To check initial state
-
+window.addEventListener('scroll', handleScroll)
+handleScroll() // To check initial state
 
 // Animation2
-const secondDiv = document.querySelector('.wallet-balance');
-console.log("its working")
+const secondDiv = document.querySelector('.wallet-balance')
+console.log('its working')
 
 const secondScroll = () => {
-  const rect = secondDiv.getBoundingClientRect();
+  const rect = secondDiv.getBoundingClientRect()
   if (rect.top < window.innerHeight && rect.bottom >= 0) {
-    secondDiv.classList.add('in-view');
+    secondDiv.classList.add('in-view')
   }
-};
+}
 
-window.addEventListener('scroll', secondScroll);
-secondScroll(); // To check initial state
+window.addEventListener('scroll', secondScroll)
+secondScroll() // To check initial state
 
 // Animation3
 
-
-
-const thirdDiv = document.querySelector('.actions');
-console.log("its working")
+const thirdDiv = document.querySelector('.actions')
+console.log('its working')
 
 const thirdScroll = () => {
-  const rect = thirdDiv.getBoundingClientRect();
+  const rect = thirdDiv.getBoundingClientRect()
   if (rect.top < window.innerHeight && rect.bottom >= 0) {
-    thirdDiv.classList.add('in-view');
+    thirdDiv.classList.add('in-view')
   }
-};
+}
 
-window.addEventListener('scroll', thirdScroll);
-thirdScroll(); // To check initial state
-
+window.addEventListener('scroll', thirdScroll)
+thirdScroll() // To check initial state
 
 // Forth Animation
 
-
-const fourthDiv = document.querySelector('.rewards');
-console.log("its working")
+const fourthDiv = document.querySelector('.rewards')
+console.log('its working')
 
 const fourthScroll = () => {
-  const rect = fourthDiv.getBoundingClientRect();
+  const rect = fourthDiv.getBoundingClientRect()
   if (rect.top < window.innerHeight && rect.bottom >= 0) {
-    fourthDiv.classList.add('in-view');
+    fourthDiv.classList.add('in-view')
   }
-};
+}
 
-window.addEventListener('scroll', fourthScroll);
-fourthScroll(); // To check initial state
-
-
+window.addEventListener('scroll', fourthScroll)
+fourthScroll() // To check initial state
 
 //  Fitfth Animation
-const fifthDiv = document.querySelector('.recent-transactions');
-console.log("its working")
+const fifthDiv = document.querySelector('.recent-transactions')
+console.log('its working')
 
 const fifthScroll = () => {
-  const rect = fifthDiv.getBoundingClientRect();
+  const rect = fifthDiv.getBoundingClientRect()
   if (rect.top < window.innerHeight && rect.bottom >= 0) {
-    fifthDiv.classList.add('in-view');
+    fifthDiv.classList.add('in-view')
   }
-};
+}
 
-window.addEventListener('scroll', fifthScroll);
-fifthScroll(); // To check initial state
-
+window.addEventListener('scroll', fifthScroll)
+fifthScroll() // To check initial state
 
 //  logout function
 
 
-
-function logout() {
-  const token = sessionStorage.getItem("token");
-  const id = sessionStorage.getItem("user_id");
-  console.log("it logged out")
-
-  $.ajax({
-    type: "GET",
-    url: "https://testing1-xpjd.onrender.com/api/auth/logout", // replace wi=th your actual logout URL
-    headers: {
-      'Authorization': `Bearer ${token}` // use backticks for template literals
-    },
-    data: {
-      'user_id': id
-    },
-    success: function (data) {
-      if (data.status === 'success') { // use triple equals for strict equality check
-        alert(data);
-        sessionStorage.clear();
-        checkSession();
-      }
-    }
-  });
-}
--- Scripts/session.js
+++ Scripts/session.js
@@ -1,16 +1,39 @@
-function checkSessionAndRedirect() {
-    // Check if the session token exists in sessionStorage
-    const sessionToken = sessionStorage.getItem('token');
+function checkSessionAndRedirect () {
+  // Check if the session token exists in sessionStorage
+  const sessionToken = sessionStorage.getItem('token')
 
-    if (!sessionToken) {
-        // If no session token, redirect to the desired page
-        window.location.href = '../start.html'; // Replace '/login.html' with your desired page
+  if (!sessionToken) {
+    // If no session token, redirect to the desired page
+    window.location.href = '../start.html' // Replace '/login.html' with your desired page
+  } else {
+    // If session exists, show the page content
+    document.body.style.display = 'block'
+  }
+}
+function logout () {
+  const token = sessionStorage.getItem('token')
+  const id = sessionStorage.getItem('user_id')
+  console.log('it logged out')
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://testing1-xpjd.onrender.com/api/auth/logout', // replace wi=th your actual logout URL
+    headers: {
+      Authorization: `Bearer ${token}` // use backticks for template literals
+    },
+    data: {
+      user_id: id
+    },
+    success: function (data) {
+      if (data.status === 'success') {
+        // use triple equals for strict equality check
+        checkSessionAndRedirect()
+        sessionStorage.clear()
+        checkSession()
+      }
     }
-    else {
-        // If session exists, show the page content
-        document.body.style.display = 'block';
-    }
-} 
+  })
+}
 // Call the function on page load
 // window.onload = function () {
 //     checkSessionAndRedirect();
